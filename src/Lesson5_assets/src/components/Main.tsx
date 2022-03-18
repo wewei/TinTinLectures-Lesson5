7f80aa15ae7eb12a7bce4200146bde482876496b
Wei Wei <weiwei@geekteahouse.cn>
2022-03-18 17:25:15 +0800

    Add a ownerStatus observable

--- src/Lesson5_assets/src/components/Main.tsx
+++ src/Lesson5_assets/src/components/Main.tsx
@@ -1,44 +1,45 @@
 import { AuthenticatedViewProps } from "./IIAuth";
-import { obPrincipal, obOwner, obMainActor, obIdentity } from "../model/Agent";
+import { obOwner, obIdentity, obOwnerStatus } from "../model/Agent";
 import Microblog from "./Microblog";
 import { useObservable } from "../hooks/Observable";
-import { nothing } from "../model/monad/Maybe";
 import { useEffect } from "react";
 
 export default function ({
   identity,
   logout,
 }: AuthenticatedViewProps): JSX.Element {
-  const owner = useObservable(obOwner);
-  const actor = useObservable(obMainActor);
-  const principal = useObservable(obPrincipal);
+  const ownerStatus = useObservable(obOwnerStatus);
 
   useEffect(() => {
     obIdentity.set(identity);
     obOwner.update();
+    // setTimeout(obOwner.update, 1000);
   }, [obIdentity, identity]);
 
-  if (owner === nothing) {
-    return (
-      <div>
-        Click <button onClick={obOwner.update}>here</button> to claim ownership
-      </div>
-    );
+  switch (ownerStatus) {
+    case "Unknown":
+      return <div>Please wait ...</div>;
+    case "Unowned":
+      return (
+        <div>
+          Click <button onClick={obOwner.update}>here</button> to claim
+          ownership
+        </div>
+      );
+    case "IsOwner":
+      return <Microblog />;
+    case "OwnedByOthers":
+      return (
+        <div>
+          <div>
+            Someone else is using this canister, are you sure to seize the
+            ownership?
+          </div>
+          <div>
+            Click <button onClick={obOwner.update}>here</button> to claim
+            ownership
+          </div>
+        </div>
+      );
   }
-
-  if (owner.toString() === principal.toString()) {
-    return <Microblog actor={actor} />;
-  }
-
-  return (
-    <div>
-      <div>
-        Someone else is using this canister, are you sure to seize the
-        ownership?
-      </div>
-      <div>
-        Click <button onClick={obOwner.update}>here</button> to claim ownership
-      </div>
-    </div>
-  );
 }
--- src/Lesson5_assets/src/model/Agent.ts
+++ src/Lesson5_assets/src/model/Agent.ts
@@ -1,11 +1,16 @@
 // Imports and re-exports candid interface
 import type { Identity } from "@dfinity/agent";
-import type { Principal } from "@dfinity/principal";
-import type { Maybe } from "./monad/Maybe";
+import { Principal } from "@dfinity/principal";
+import { fromMaybe, Maybe } from "./monad/Maybe";
 
 import { createActor } from "../../../declarations/Lesson5";
 import { MaybeMonad as M, nothing } from "./monad/Maybe";
-import { ObservableMonad as O, encapsulated } from "./monad/Observable";
+import {
+  ObservableMonad as O,
+  encapsulated,
+  mutable,
+  Observable,
+} from "./monad/Observable";
 
 // LESSON5_CANISTER_ID is replaced by webpack based on node environment
 export const canisterId: string = process.env.LESSON5_CANISTER_ID as string;
@@ -19,11 +24,7 @@ export type MicroblogActor = ReturnType<typeof createActor>;
 
 export const obIdentity = encapsulated(
   nothing as Maybe<Identity>,
-  ({ notify }) => ({
-    set: (i) => {
-      notify(i);
-    },
-  })
+  ({ notify }) => ({ set: notify })
 );
 
 export const obActorFor = O.fmapU(obIdentity, M.fmap(withIdentity));
@@ -38,11 +39,29 @@ export const obPrincipal = O.fmapU(
   M.fmap((id) => id.getPrincipal())
 );
 
-export const obOwner = encapsulated(nothing as Maybe<Principal>, (owner) => {
-  const update = () =>
-    M.fmapU(obMainActor.current(), async (actor) => {
-      console.log(actor);
-      owner.notify((await actor.get_owner())[0] ?? nothing);
-    });
-  return { update };
-});
+export const obOwner = encapsulated(
+  nothing as Maybe<Principal | null>,
+  (owner) => ({
+    update: () =>
+      M.fmapU(obMainActor.current(), async (actor) => {
+        owner.notify((await actor.get_owner())[0] ?? nothing);
+      }),
+  })
+);
+
+export type OwnerStatus = "Unknown" | "Unowned" | "OwnedByOthers" | "IsOwner";
+export const obOwnerStatus = O.bindU(
+  obOwner,
+  (o): Observable<OwnerStatus> =>
+    o === nothing
+      ? O.pure("Unknown")
+      : o === null
+      ? O.pure("Unowned")
+      : O.fmapU(obPrincipal, (p) =>
+          p === nothing
+            ? "Unknown"
+            : p.toString() === o.toString()
+            ? "IsOwner"
+            : "OwnedByOthers"
+        )
+);
